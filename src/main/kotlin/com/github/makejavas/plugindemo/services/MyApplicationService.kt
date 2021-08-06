5799bdfc944ba70069f52f105712ba3f7b6ae928
GitHub Action <action@github.com>
2021-08-06 02:19:23 +0000

    Template cleanup

--- src/main/kotlin/com/github/makejavas/plugindemo/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/makejavas/plugindemo/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.makejavas.plugindemo.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.makejavas.plugindemo.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/makejavas/plugindemo/services/MyProjectService.kt
+++ src/main/kotlin/com/github/makejavas/plugindemo/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.makejavas.plugindemo.services
 
+import com.github.makejavas.plugindemo.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
